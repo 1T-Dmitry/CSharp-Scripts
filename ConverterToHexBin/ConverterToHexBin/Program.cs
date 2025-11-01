809d908a2be2592124e8e9c88b50e99d8a8769ac
Dima Rodoh <rodohlebov.dima@gmail.com>
2025-11-02 02:02:49 +0700

    Add ConverterToHexBin

---
+++ ConverterToHexBin/ConverterToHexBin/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter text to convert it to binary and hexadecimal representation:");
+string text = Console.ReadLine() ?? "";
+Console.WriteLine();
+
+Console.WriteLine("Binary representation:");
+foreach (char c in text)
+{
+    Console.Write(Convert.ToString(c, 2));
+}
+Console.Write("\n\n");
+
+Console.WriteLine("Hexadecimal representation:");
+foreach (char c in text)
+{
+    Console.Write(Convert.ToString(c, 16));
+}
+Console.WriteLine();
